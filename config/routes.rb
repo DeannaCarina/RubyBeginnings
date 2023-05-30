ebc2ea28dac8fb727dd2778eaeada3e2b9acf136
DeannaCarina <deannacarina@hotmail.com>
2023-05-30 15:41:13 +0000

    Add: Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
-Rails.application.routes.draw do
-  #GET /about
-  get "about", to: "about#index"
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+Rails.application.routes.draw do
+  root to: "main#index"
+  get "about", to: "about#index"
+  
+
 end
